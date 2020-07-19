b2c548a4036897c07c990ee780b56c3b5d2bcee9
araneoAnicet <araneoanicet@gmail.com>
2020-07-19 15:10:12 +0100

    fix Arguments class

--- Arguments/Arguments.h
+++ Arguments/Arguments.h
@@ -2,13 +2,13 @@
 #include <string>
 #include <vector>
 
-class Arguments 
-{
+class Arguments {
 private:
     std::string name;
-    std::vector<Argument*> Arguments;
-    std::vector<Entity*> Entitys;
-    std::vector<FloatArgument*> FloatArgument;
-    std::vector<NumberArgument*> NumberArgument;
-    std::vector<StringArgument*> StringArgument;
-}
+    std::vector<Entity*> Entities;
+    std::vector<FloatArgument*> float_arguments;
+    std::vector<NumberArgument*> number_arguments;
+    std::vector<StringArgument*> string_arguments;
+    std::vector<EntityArgument*> entity_arguments;
+    std::vector<BoolArgument*> bool_arguments;
+};
