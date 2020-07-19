6e98873772d7385f1b2fa5c988d00ef268fdb454
araneoAnicet <araneoanicet@gmail.com>
2020-07-19 15:11:04 +0100

    remove Entities

--- Arguments/Arguments.h
+++ Arguments/Arguments.h
@@ -5,7 +5,6 @@
 class Arguments {
 private:
     std::string name;
-    std::vector<Entity*> Entities;
     std::vector<FloatArgument*> float_arguments;
     std::vector<NumberArgument*> number_arguments;
     std::vector<StringArgument*> string_arguments;
