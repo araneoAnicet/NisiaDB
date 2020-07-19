f5490b8ed9ff6dbdb699e39f1436b621b8356aa7
araneoAnicet <araneoanicet@gmail.com>
2020-07-19 15:56:12 +0100

    add constructor

---
+++ Database.cpp
@@ -0,0 +1,6 @@
+#include "Database.h"
+
+Database::Database(std::string name) {
+    this->name = name;
+}
+
--- Database.h
+++ Database.h
@@ -9,6 +9,7 @@ private:
     std::string name;
     std::vector<Table*> tables;
 public:
+    Database(std::string name);
     void add_table(Table* table);
     std::vector<Table*> get_tables();
 };
