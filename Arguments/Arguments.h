2a8c3107e2c0dcca48299d222e5d362b78000e27
araneoAnicet <araneoanicet@gmail.com>
2020-07-19 15:12:02 +0100

    add constructor

--- Arguments/Arguments.h
+++ Arguments/Arguments.h
@@ -4,10 +4,11 @@
 
 class Arguments {
 private:
-    std::string name;
     std::vector<FloatArgument*> float_arguments;
     std::vector<NumberArgument*> number_arguments;
     std::vector<StringArgument*> string_arguments;
     std::vector<EntityArgument*> entity_arguments;
     std::vector<BoolArgument*> bool_arguments;
+public:
+    Arguments(Arguments* arguments);
 };
