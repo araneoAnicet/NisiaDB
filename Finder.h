ac736f06abc72e2b4f2ad28c4456bd92ec8c1a01
Dmitry Morozov <d.umutrui@yandex.ru>
2020-07-20 15:09:47 +0300

    add includes

--- Finder.h
+++ Finder.h
@@ -1,11 +1,13 @@
 #pragma once
 #include "EntityContainer.h"
 #include "Selector.h"
+#include "Entity.h"
+#include "Table.h"
 
 class Finder{
 private:
 friend class Table;
-    std::vector<Entity*> *entities;
+    std::vector<Entity*> *entities;  
     void set_arguments(Arguments* arguments);
 public:
     EntityContainer by_id(unsigned int id);
