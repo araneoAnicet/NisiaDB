322295dcf14ddc661c94fa5b1316992c470e5fb6
Dmitry Morozov <d.umutrui@yandex.ru>
2020-08-10 22:06:59 +0300

    add relationship between LinkedList and GarbageCollector

--- GarbageCollector.h
+++ GarbageCollector.h
@@ -1,12 +1,14 @@
 #pragma once
 #include "Notified.h"
 #include "Collectable.h"
+#include "LinkedList.h"
 
 class Collectalbe;
 class GarbageCollector : public Notified {
 private:
     static GarbageCollector* instance;
     GarbageCollector();
+    LinkedList<Collectable*> data;
 public:
     static GarbageCollector* get_instance();
     static void init();
