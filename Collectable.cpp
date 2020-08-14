e7cdf7dfd64bd634bba6e5caf155b57a39536ab5
araneoAnicet <araneoanicet@gmail.com>
2020-08-14 19:47:05 +0100

    add get and push methods

--- Collectable.cpp
+++ Collectable.cpp
@@ -3,3 +3,11 @@
 void Collectable::notify_initialization() {
     GarbageCollector::get_instance()->notify_creation(this);
 }
+
+void Collectable::push_node(CollectableNode* stack_node) {
+    this->nodes_pointing_to.push(stack_node);
+}
+
+std::stack<CollectableNode*> Collectable::get_nodes_pointing_to() {
+    return this->nodes_pointing_to;
+}
--- Collectable.h
+++ Collectable.h
@@ -2,14 +2,24 @@
 #include <stack>
 #include "GarbageCollector.h"
 
+class CollectableNode {
+    public:
+    CollectableNode(Collectable* ptr) {
+        this->ptr = ptr;
+    }
+    CollectableNode();
+    private:
+    Collectable* ptr = nullptr;
+};
+
 class GarbageCollector;
 class Collectable {
 private:
-    typedef struct {
-        Collectable* ptr = nullptr;
-    } Node;
-    std::stack<Node*> nodes_pointing_to;
+    std::stack<CollectableNode*> nodes_pointing_to;
+    CollectableNode* me = new CollectableNode(this);
 public:
     virtual void remove() = 0;
     void notify_initialization();
+    void push_node(CollectableNode* stack_node);
+    std::stack<CollectableNode*> get_nodes_pointing_to();
 };
