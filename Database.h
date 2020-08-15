654d6bbacbde502a35bba1803185e37daec6d018
araneoAnicet <araneoanicet@gmail.com>
2020-08-15 15:54:45 +0100

    add Database id

--- Database.h
+++ Database.h
@@ -5,6 +5,7 @@
 #include <iostream>
 #include "Table.h"
 #include "CanShow.h"
+#include "Identity.h"
 
 class Table;
 class Database : public CanShow {
@@ -12,6 +13,7 @@ private:
     std::string name;
     std::vector<Table*> tables;
 public:
+    Identity id;
     Database(std::string name);
     void add_table(Table* table);
     std::vector<Table*> get_tables();
