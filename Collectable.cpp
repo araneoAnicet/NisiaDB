7f92fd3c8a334a84ca3d47ea74af4cfe104cd2c6
araneoAnicet <araneoanicet@gmail.com>
2020-08-16 14:19:46 +0100

    add methods for remove

--- Collectable.cpp
+++ Collectable.cpp
@@ -12,10 +12,19 @@ std::stack<CollectableNode*> Collectable::get_nodes_pointing_to() {
     return this->nodes_pointing_to;
 }
 
+// notify GC about object removing
+void Collectable::__delete() {
+    GarbageCollector::get_instance()->get_notification();
+}
+
 bool Collectable::get_remove_marker(){
     return this->remove_marker;
 }
 
-void Collectable::set_remove_marker(bool value){
-    this->remove_marker = value;
+void Collectable::set_remove_marker(){
+    this->remove_marker = true;
+}
+
+CollectableNode::CollectableNode(Collectable* ptr) {
+    this->ptr = ptr;
 }
--- Collectable.h
+++ Collectable.h
@@ -3,26 +3,24 @@
 #include "GarbageCollector.h"
 
 class CollectableNode {
-    public:
-    CollectableNode(Collectable* ptr) {
-        this->ptr = ptr;
-    }
-    CollectableNode();
-    private:
-    Collectable* ptr = nullptr;
+private:
+    Collectable* ptr;
+public:
+    CollectableNode(Collectable* ptr);
 };
 
 class GarbageCollector;
 class Collectable {
-private:
-    bool remove_marker;
+protected:
+    bool remove_marker = false;
     std::stack<CollectableNode*> nodes_pointing_to;
     CollectableNode* me = new CollectableNode(this);
+    void __delete();
 public:
     virtual void remove() = 0;
     void notify_initialization();
     void push_node(CollectableNode* stack_node);
     std::stack<CollectableNode*> get_nodes_pointing_to();
     bool get_remove_marker();
-    void set_remove_marker(bool value);
+    void set_remove_marker();
 };
