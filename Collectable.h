c29c53c929448d6e337d283f57168fd1564baf09
araneoAnicet <araneoanicet@gmail.com>
2020-08-16 16:15:19 +0100

    Revert "add methods for remove"
    This reverts commit 7f92fd3c8a334a84ca3d47ea74af4cfe104cd2c6.

--- Collectable.h
+++ Collectable.h
@@ -3,24 +3,26 @@
 #include "GarbageCollector.h"
 
 class CollectableNode {
-private:
-    Collectable* ptr;
-public:
-    CollectableNode(Collectable* ptr);
+    public:
+    CollectableNode(Collectable* ptr) {
+        this->ptr = ptr;
+    }
+    CollectableNode();
+    private:
+    Collectable* ptr = nullptr;
 };
 
 class GarbageCollector;
 class Collectable {
-protected:
-    bool remove_marker = false;
+private:
+    bool remove_marker;
     std::stack<CollectableNode*> nodes_pointing_to;
     CollectableNode* me = new CollectableNode(this);
-    void __delete();
 public:
     virtual void remove() = 0;
     void notify_initialization();
     void push_node(CollectableNode* stack_node);
     std::stack<CollectableNode*> get_nodes_pointing_to();
     bool get_remove_marker();
-    void set_remove_marker();
+    void set_remove_marker(bool value);
 };
